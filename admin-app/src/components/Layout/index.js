d29b48b5a559200d7f782a0b8f0e06a131ecb886
flyhighrishi <rishikeshshukla47@gmail.com>
2021-03-10 18:39:29 +0530

    Admin Dashboard

---
+++ admin-app/src/components/Layout/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from '../Header';
+
+const Layout = (props) => {
+    return (
+        <>
+            <Header/>
+            
+                {props.children}
+            
+            
+        </>
+    );
+}
+
+export default Layout
